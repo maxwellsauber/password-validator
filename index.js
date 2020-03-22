3fb8ff936100554e7777204454e57066ee2a64e2
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-03-22 15:20:24 -0400

    refactor

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 const validatePassword = (password) => {
-  const upperCaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-    'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']  
+  const alphaChar = 'abcdefghijklmnopqrstuvwxyz' //add more for i18n
+  const specialChar = '~!@#$%^&*()_+'
   
-  const lowerCaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
- 
-  const specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
-    
-  if (password.length < 8) {return false}
-  if (!checkNumber(password)) { return false }
-  if (!checkCharacters(password, upperCaseCharacters)) {return false}
-  if (!checkCharacters(password, lowerCaseCharacters)) {return false}
-  if (!checkCharacters(password, specialCharacters)) {return false}
+  if (password.length < 8) { return false }
+  if (!checkNum(password)) { return false }
+  if (!checkChar(password, alphaChar.toUpperCase().split(''))) { return false }
+  if (!checkChar(password, alphaChar.split(''))) { return false }
+  if (!checkChar(password, specialChar.split(''))) { return false }
   return true
 }
 
-const checkCharacters = (password, characters) => {
+const checkChar = (password, characters) => {
   for (var i = 0; i < password.length; i++) {
     if (characters.includes(password.charAt(i))) {
       return true
@@ -23,11 +18,12 @@ const checkCharacters = (password, characters) => {
   }
 }
 
-const checkNumber = (password) => {
+const checkNum = (password) => {
   for (var i = 0; i < password.length; i++) {
     if (!isNaN(password.charAt(i))) {
       return true
     }
   }
 }
+
 module.exports = validatePassword
